d50a36b3c88bb9282ad3eb8cdd07818add7c2268
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-29 18:50:58 -0500

    Now the user can type in restart, new or done. And fixed spacing in all txt files

--- The_Machine.rb
+++ The_Machine.rb
@@ -1,3 +1,39 @@
+# categories = {
+#   "1" => "If, Else, Elsif",
+#   "2" => "Strings",
+#   "3" => "Built-in Methods for strings and numbers",
+#   "4" => "Loop basics",
+#   "5" => "Array basics",
+#   "6" => "Hash basics",
+#   "7" => "Functions",
+#   "8" => "Mapping Arrays to new ones",
+#   "9" => "Array >> Shoveling",
+#   "10" => "Reduce an Array",
+#   "11" => "Data Type Conversions: GitHub link broken_made up questions",
+#   "12" => "Loop within a Loop"
+# }
+
+# puts "Please select a category by entering a number:"
+# categories.each do |key, value|
+#   puts "#{key}. #{value}"
+# end
+
+# selected_category = gets.chomp
+# puts "You have selected category #{selected_category}: #{categories[selected_category]}"
+
+# filename = "#{selected_category}.txt"
+# if File.exist?(filename)
+#   questions = File.readlines(filename, chomp: true)
+#   if questions.any?
+#     puts "Here is a random question from category #{selected_category}:"
+#     puts questions.sample
+#   else
+#     puts "There are no questions in this category."
+#   end
+# else
+#   puts "Invalid category selected."
+# end
+
 categories = {
   "1" => "If, Else, Elsif",
   "2" => "Strings",
@@ -13,23 +49,46 @@ categories = {
   "12" => "Loop within a Loop"
 }
 
-puts "Please select a category by entering a number:"
-categories.each do |key, value|
-  puts "#{key}. #{value}"
+def display_question(category)
+  filename = "#{category}.txt"
+  if File.exist?(filename)
+    questions = File.readlines(filename, chomp: true)
+    if questions.any?
+      puts "Here is a random question from category #{category}:"
+      puts ""
+      puts questions.sample
+    else
+      puts "There are no questions in this category."
+    end
+  else
+    puts "Invalid category selected."
+  end
 end
 
-selected_category = gets.chomp
-puts "You have selected category #{selected_category}: #{categories[selected_category]}"
-
-filename = "#{selected_category}.txt"
-if File.exist?(filename)
-  questions = File.readlines(filename, chomp: true)
-  if questions.any?
-    puts "Here is a random question from category #{selected_category}:"
-    puts questions.sample
-  else
-    puts "There are no questions in this category."
+loop do
+  puts "Please select a category by entering a number:"
+  categories.each do |key, value|
+    puts "#{key}. #{value}"
   end
-else
-  puts "Invalid category selected."
+
+  selected_category = gets.chomp
+  puts "You have selected category #{selected_category}: #{categories[selected_category]}"
+
+  display_question(selected_category)
+
+  loop do
+    puts ""
+    puts ""
+    puts "Type 'done' to end the program, 'restart' to restart the program, or 'new' to get a new question in the same category."
+    response = gets.chomp.downcase
+    if response == "done"
+      exit
+    elsif response == "restart"
+      break
+    elsif response == "new"
+      display_question(selected_category)
+    else
+      puts "Invalid response."
+    end
+  end
 end
